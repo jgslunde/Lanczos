239d3ba88dcb7281e1417c4d4adec61dd3cb76af
asdfbat <jonas.s.lunde@outlook.com>
2019-03-04 19:46:11 -0800

    Implemented 27-point Laplacian

--- Python/Hamiltonian.py
+++ Python/Hamiltonian.py
@@ -15,6 +15,13 @@ class Hamiltonian:
         self.y = np.linspace(-L//2, L//2, N)
         self.z = np.linspace(-L//2, L//2, N)
 
+        # Setting up 7-point stencil and weights.
+        self.neighbors_relative_7point = np.array([[0,0,0], [-1,0,0], [0,-1,0], [0,0,-1], [1,0,0], [0,1,0], [0,0,1]])
+        self.weights_7point = np.ones(7); self.weights_7point[0] = -6
+
+        # Setting up 27-point stencil and weights.
+        self.neighbors_relative_27point = np.array([[i,j,k] for i in range(-1,2) for j in range(-1,2) for k in range(-1,2)])
+        self.weights_27point = self.get_weights_27point()
 
 
     def create_sparse_Hamiltonian(self):
@@ -36,18 +43,17 @@ class Hamiltonian:
 
     def create_sparse_T(self):
         print("+++ Setting up sparse laplacian matrix T.")
-        T_factor = self.T_factor
-        N, stencil = self.N, self.stencil
+        T_factor, N = self.T_factor, self.N
         row_ind = []; col_ind = []; data = []
         for i in tqdm(range(N**3)):
-            neighbors = stencil(i)
-            for neighbor in neighbors:
+            neighbors, weights = self.Laplacian_27point(i)
+            for j, neighbor in enumerate(neighbors):
                 row_ind.append(i)
                 col_ind.append(neighbor)
-                data.append(T_factor)
-            row_ind.append(i)
-            col_ind.append(i)
-            data.append(-6*T_factor)
+                data.append(T_factor*weights[j])
+            #row_ind.append(i)
+            #col_ind.append(i)
+            #data.append(-6*T_factor*weights[i])
         self.T_sparse = scipy.sparse.csc_matrix((data, (row_ind, col_ind)), shape=(N**3,N**3))
 
 
@@ -103,7 +109,49 @@ class Hamiltonian:
         assert (x_prev, x_next, y_prev, y_next, z_prev, z_next) == (N-1, 1, (N-1)*N, N, (N-1)*N**2, N**2)
 
 
-    def Laplacian(self, i):
+    def Laplacian_7point(self, i):
         # Returns the points with weights of the laplacian.
-        pass
+        x, y, z = self.ravel_i(i)
+        neighbors_relative = self.neighbors_relative_7point
+        neighbors = neighbors_relative + np.array([x,y,z])
+        for neighbor in neighbors:
+            for i in range(3):
+                if neighbor[i] == self.N:
+                    neighbor[i] = 0
+                elif neighbor[i] == -1:
+                    neighbor[i] = self.N-1
+        neighbors_i = [self.unravel_xyz(x,y,z) for x,y,z in neighbors]
+        return neighbors_i, self.weights_7point
 
+
+    def Laplacian_27point(self, i):
+        x, y, z = self.ravel_i(i)
+        neighbors_relative = self.neighbors_relative_27point
+        neighbors = neighbors_relative + np.array([x,y,z])
+        for neighbor in neighbors:
+            for i in range(3):
+                if neighbor[i] == self.N:
+                    neighbor[i] = 0
+                elif neighbor[i] == -1:
+                    neighbor[i] = self.N-1
+        neighbors_i = [self.unravel_xyz(x,y,z) for x,y,z in neighbors]
+        return neighbors_i, self.weights_27point
+
+
+    def get_weights_27point(self):
+        weights_27point = np.zeros(27)
+        for i in range(27):
+            if (self.neighbors_relative_27point[i] == 0).all():  # Center
+                weights_27point[i] = -44/3
+            elif (self.neighbors_relative_27point[i] != 0).all():  # Corners
+                weights_27point[i] = 1.0/3
+            elif ( np.sum(self.neighbors_relative_27point[i] != 0) > 1):  # Edge
+                weights_27point[i] = 1.0/2
+            else:
+                weights_27point[i] = 1.0
+        weights_27point = weights_27point*3.0/13
+        return weights_27point
+
+
+if __name__ == "__main__":
+    TEST = Hamiltonian(1,1,1,1)
