7225bc06e5e93e62bdd23f2a4473f7927ee32984
asdfbat <jonas.s.lunde@outlook.com>
2019-04-05 02:38:24 -0700

    added test plot for nearest neighbors search to testing file

--- Python/Irregular/testing.py
+++ Python/Irregular/testing.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import time
 from tqdm import trange
 from IrrGrid import IrrGrid
+from symetry import FindMirrorSymetricPoints
 
 
 def Test_Plot_IsCloseToEdge():
@@ -27,6 +28,50 @@ def Test_Plot_IsCloseToEdge():
 
 
 
+def Test_Plot_GetNearbyPoints():
+    N = 30
+    L = 25
+    box_depth = 3
+    grid = IrrGrid(N, L)
+    grid.SetupBoxes(box_depth=box_depth)
+
+
+    idx = 2334
+    center_coord = grid.point_coords[idx]
+    print("COORDS:", center_coord)
+    print(grid.IsCloseToEdge(idx, 1))
+    print()
+    t0 = time.time()
+    neighbor_depth = 3
+    neighbors_idxs = grid.GetNearbyPoints(idx, neighbor_depth)
+    print(time.time() - t0)
+    neighbors = grid.point_coords[neighbors_idxs]
+
+    asdf = FindMirrorSymetricPoints(neighbors-center_coord)
+    print(len(neighbors))
+    print(len(asdf))
+
+    print(neighbors)
+    neighbors2D = neighbors[:,:2]
+    fig, ax = plt.subplots(3,3, figsize=(20,20))
+    for i in range(-neighbor_depth, neighbor_depth + 1):
+        k = i + neighbor_depth
+        z = center_coord[2] + int(i)
+        print(z)
+        for j in range(neighbors.shape[0]):
+            if neighbors[j,2] == z:
+                ax[k//3, k%3].scatter(*neighbors[j,:2], c="b")
+                ax[k//3, k%3].set_title(f"Z = {z}")
+                ax[k//3, k%3].set_xlim(center_coord[0]-neighbor_depth*2, center_coord[0]+neighbor_depth*2)
+                ax[k//3, k%3].set_ylim(center_coord[1]-neighbor_depth*2, center_coord[1]+neighbor_depth*2)
+        if i == 0:
+            ax[k//3, k%3].scatter(*center_coord[:2], c="r")
+        # plt.axhline(y=x*N//box_depth-0.5, ls="--", c="y")
+        # plt.axvline(x=x*N//box_depth-0.5, ls="--", c="y")
+    plt.show()
+
+
+
 
 
 
@@ -38,4 +83,5 @@ def Test_Plot_IsCloseToEdge():
 
 
 if __name__ == "__main__":
+    Test_Plot_GetNearbyPoints()
     Test_Plot_IsCloseToEdge()
