0c108a37a41201e01ef656cdffe852fe05cb02fa
asdfbat <jonas.s.lunde@outlook.com>
2019-04-25 05:05:36 -0700

    implemented new displacement tools, with hashes, to get allowed displacements in a searchcube

--- Python/Irregular/tools.py
+++ Python/Irregular/tools.py
@@ -1,6 +1,6 @@
 """ File containing random usefull functions that doesn't really belong anywhere in particular."""
 import numpy as np
-from itertools import repeat
+from itertools import product
 
 
 def unravel_ijk(i, j, k, N):
@@ -26,21 +26,52 @@ def get_relative_positions(center_pos, pos_array, N):
     return delta_pos
 
 
-def get_displacement_stencil(D, on_grid, a, stretch_offgrid=True):
-    """INPUT:
-    D = Int. Depth of points in each direction.
-    on_grid = [dims] boolean array, representing if each dimension is on or off grid.
-    a = default spacing in each dimension.
-    stretch_offgrid = Option to include +1 depth in offgrid direction.
-    OUTPUT:
-    """
 
-    disp_options = []
-    for dim in range(3):
-        if on_grid[dim]:
-            disp_options.append(np.arange(-D, D+1, a))
-        else:
-            disp_options.append(np.concatenate((np.arange(-D, 0, a), np.arange(a, D+1, a)), axis=None))
+hashdict = {}
+def get_symetric_displacement_stencil(D, d):
+    # Returns a [(2*D+1)^d, d] array with all possible displacements around the origin, up to "D" in each direction.
+    hash = D+d*128  # Creating a unique hash for the inputs.
+    if hash in hashdict:
+        return hashdict[hash]
+    else:
+        disp = np.array(list(product(range(-D, D+1), repeat=d)))
+        hashdict[hash] = disp
+    return disp
 
-        disp = repeat(disp_options)
-        print(disp)
+
+hashdict2 = {}
+def get_displacement_stencil(searchcube, d):
+    hash = searchcube.tobytes()
+    if hash in hashdict2:
+        return hashdict2[hash]
+    else:
+        disp = np.array(list(product(searchcube, repeat=d)))
+        hashdict2[hash] = disp
+    return disp
+
+
+
+# def get_displacement_stencil(D, on_grid, a, stretch_offgrid=True):
+#     """INPUT:
+#     D = Int. Depth of points in each direction.
+#     on_grid = [dims] boolean array, representing if each dimension is on or off grid.
+#     a = default spacing in each dimension.
+#     stretch_offgrid = Option to include +1 depth in offgrid direction.
+#     OUTPUT:
+#     """
+
+#     disp_options = []
+#     for dim in range(3):
+#         if on_grid[dim]:
+#             disp_options.append(np.arange(-D, D+1, a))
+#         else:
+#             disp_options.append(np.concatenate((np.arange(-D, 0, a), np.arange(a, D+1, a)), axis=None))
+
+#         disp = np.array(list(repeat(disp_options)))
+#     return disp
+
+
+
+if __name__ == "__main__":
+    for i in range(int(1e6)):
+        disp = get_symetric_displacement(2, 3)
